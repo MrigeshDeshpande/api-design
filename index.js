94962c93e99d6d25df29ba66c32d5635fab7481b
Mrigesh Deshpande <mrigeshdeshpande246@gmail.com>
2022-12-30 19:52:12 +0530

    Using service orientated approach

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
-var app=require('./server/serve');
-app.listen(5000);
-console.log('See you on port 5000');
+
+/*
+    intro point for our server.
+
+    PRO-TIP: if you have an index.js file on the root of a folder in node
+    you can just require that folder and node will automatically require the index.js on the root
+    setup config first before anything by requiring it
+    */
+var config = require('./server/config/config');
+var app = require('./server/server');
+/*
+    logger is a wrapper around console.log that adds color,
+    logs objects as json and can be conditionally turned off
+    so you don't have to erase all calls to it
+*/
+
+var logger = require('./server/util/logger');
+
+app.listen(config.port);
+logger.log('listening on http://localhost:' + config.port);
+
