7e0c15634277b9016d521e19e4560d33a0f1415c
Mrigesh Deshpande <mrigeshdeshpande246@gmail.com>
2022-12-30 11:19:29 +0530

    Testing Complete

---
+++ index.js
@@ -0,0 +1,3 @@
+var app=require('./server/serve');
+app.listen(5000);
+console.log('See you on port 5000');
--- server/serve.js
+++ server/serve.js
@@ -3,7 +3,8 @@ var app = express();
 var bodyParser = require('body-parser');
 var _ = require('lodash');
 
-var tigerRouter = require('./tigers');
+var tigerRouter = require('./tigers')
+
 app.use(express.static('client'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -17,6 +18,8 @@ app.use((err, req, res, next) => {
     }
 })
 
-app.listen(5000, () => {
-    console.log('listening on port 5000');
-});
+// app.listen(5000, () => {
+//     console.log('listening on port 5000');
+// });
+//remove listen and export 
+module.exports=app;
